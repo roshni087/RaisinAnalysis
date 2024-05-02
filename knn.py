9d7e26136c665c4f669d8f9c3b3d5e9d569e0d10
roshni087 <168722070+roshni087@users.noreply.github.com>
2024-05-02 14:52:48 +0530

    Update knn.py

--- knn.py
+++ knn.py
@@ -19,7 +19,7 @@ data_scaled = scaler.fit_transform(data.iloc[:, :-1])
 scaler = MinMaxScaler()
 data_normalized = scaler.fit_transform(data_scaled)
 
-#split data into training and testing sets
+# split data into training and testing sets
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(data_normalized[:, :-1], data['Class'], test_size=0.2, random_state=42)
 
